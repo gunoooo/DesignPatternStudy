39f5d8116e51d61a8adeb43fc11ccdf269a8dbfe
박건우 <39177603+rjsdnqkr1@users.noreply.github.com>
2021-05-19 00:17:56 +0900

    [FIX] DecoratorPattern

--- DecoratorPattern.playground/Contents.swift
+++ DecoratorPattern.playground/Contents.swift
@@ -9,3 +9,17 @@ private var mainAccountList: [계좌Model] = {
         )
     ).전체계좌조회()
 }()
+
+private var top3AccountList: [계좌Model] = {
+    return KookminAccountDecorator(
+        IBKAccountDecorator(
+            ShinhanAccount()
+        )
+    ).전체계좌조회()
+}()
+
+private var suggestAccountList: [계좌Model] = {
+    return KakaoAccountDecorator(
+        ShinhanAccount()
+    ).전체계좌조회()
+}()
